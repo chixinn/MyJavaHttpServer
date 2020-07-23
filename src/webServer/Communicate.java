39feb9801ba9dcabe2250f47df07c6bb264bfab5
chixinning <xinning_chi@163.com>
2020-07-23 08:42:22 +0800

    ERROR

--- src/webServer/Communicate.java
+++ src/webServer/Communicate.java
@@ -46,45 +46,49 @@ public class Communicate extends Thread{
             //获取文件路径
             String fileName=getResourcePath(msg);
             //如果是shutdown就退出
-            if (fileName.equals("shutdown")){
+            if (fileName.equals("/shutdown")){
                 socket.close();
                 System.exit(0);
             }
-            System.out.print("用户请求的文件名是"+fileName);
-            String filePath=ROOT+fileName;
-            File file = new File(filePath);
-            System.out.print("用户请求的文件路径是"+file.getPath());
-            if(file.exists()){
-                //如果是有效路径
-                System.out.println(fileName+" start send");
+            else{
+                System.out.print("用户请求的文件名是"+fileName);
+                String filePath=ROOT+fileName;
+                File file = new File(filePath);
+                System.out.print("用户请求的文件路径是"+file.getPath());
+                if(file.exists()){
+                    //如果是有效路径
+                    System.out.println(fileName+" start send");
 
-                out.println("HTTP/1.0 200 OK");
-                out.println("MIME_version:1.0");
-                out.println("Content_Type:text/html");
-                int len = (int) file.length();
-                out.println("Content_Length:"+len);
-                out.println("");//报文头和信息之间要空一行
-                //发送文件Page
-                sendFile(out,file);
+                    out.println("HTTP/1.0 200 OK");
+                    out.println("MIME_version:1.0");
+                    out.println("Content_Type:text/html");
+                    int len = (int) file.length();
+                    out.println("Content_Length:"+len);
+                    out.println("");//报文头和信息之间要空一行
+                    //发送文件Page
+                    sendFile(out,file);
+                }
+                else
+                {
+                    System.out.println("\nResources not avaliable");
+
+                    out.println("HTTP/1.0 400 file not found");
+                    out.println("MIME_version:1.0");
+                    out.println("Content_Type:text/html");
+                    int len = (int) file.length();
+                    out.println("Content_Length:"+len);
+                    out.println("");//报文头和信息之间要空一行
+                    //发送文件ErrorPage
+                    String ErrorfilePage=ROOT+"error.html";
+                    File errorfile=new File(ErrorfilePage);
+                    sendFile(out,errorfile);
+                    out.flush();
+
+                }
+                socket.close();
+
             }
-            else
-            {
-                System.out.println("Resources not avaliable");
 
-                out.println("HTTP/1.0 400 file not found");
-                out.println("MIME_version:1.0");
-                out.println("Content_Type:text/html");
-                int len = (int) file.length();
-                out.println("Content_Length:"+len);
-                out.println("");//报文头和信息之间要空一行
-                //发送文件ErrorPage
-                String ErrorfilePage=ROOT+"error.html";
-                File errorfile=new File(ErrorfilePage);
-                sendFile(out,errorfile);
-                out.flush();
-
-            }
-            socket.close();
 
 
         }catch(Exception e){
